c9bcddb2f3625009299bbba4da05800562a65b12
Arnav Sharma <sharma.arnav2016@gmail.com>
2024-11-17 16:54:06 -0800

    added imports/gitignore

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+// these are the imports I think I will need for now
+int main() {
+    
+}
+
+
