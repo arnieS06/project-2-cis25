d08195a56a13c9942e8eeebcaa3dd990e56d654b
arnieS06 <sharma.arnav2016@gmail.com>
2024-11-17 18:37:27 -0800

    Updated

--- main.cpp
+++ main.cpp
@@ -4,11 +4,32 @@
 #include <fstream>
 #include <ctime>
 #include <cstdlib>
+#include "Structs.h"
+#include "wordList.h"
 using namespace std;
+
+vector<string> loadWords(const string& filename) {
+    vector<string> words;
+    ifstream file(filename);
+    string word;
+
+    while (file >> word) {
+        words.push_back(word);
+    }
+
+    return words;
+}
+
 
 // these are the imports I think I will need for now
 int main() {
-    
+    GameState game;
+
+    game.listOfWords = loadWords("wordlist.txt");
+
+    for (const auto& word : game.listOfWords) {
+        cout << word << endl;
+    }
 }
 
 
