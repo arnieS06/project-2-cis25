a10a0a862d3b2d8b1d558fa84b5cbb303e87b14d
arnieS06 <sharma.arnav2016@gmail.com>
2024-11-17 18:37:42 -0800

    Updated file

--- Structs.h
+++ Structs.h
@@ -1,2 +1,17 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+struct GameState {
+	std::vector<std::string> listOfWords;
+	std::string targetWord;
+	int remainingAttempts;
+	std::vector<std::string> guesses;
+};
+
+enum Feedback { Correct, Misplacyed, Incorrect };
+
+struct WordUtils {
+
+};
